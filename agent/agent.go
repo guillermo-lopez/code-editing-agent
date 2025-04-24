10fd0a728e12909fa054c881ec7b3c8afeadfbc6
Guillermo Lopez <glopez686@gmail.com>
2025-04-24 19:08:35 -0400

    update agent

--- agent/agent.go
+++ agent/agent.go
@@ -3,7 +3,9 @@ package agent
 import (
 	. "code-editing-agent/tools"
 	"context"
+	"encoding/json"
 	"fmt"
+
 	"github.com/anthropics/anthropic-sdk-go"
 )
 
@@ -29,16 +31,19 @@ func (a *Agent) Run(ctx context.Context) error {
 	conversation := []anthropic.MessageParam{}
 
 	fmt.Println("Chat with Claude (use 'ctrl-c' to quit)")
+	readUserInput := true
 
 	for {
-		fmt.Print("\u001b[94mYou\u001b[0m: ")
-		userInput, ok := a.getUserMessage()
-		if !ok {
-			break
-		}
+		if readUserInput {
+			fmt.Print("\u001b[94mYou\u001b[0m: ")
+			userInput, ok := a.getUserMessage()
+			if !ok {
+				break
+			}
 
-		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
-		conversation = append(conversation, userMessage)
+			userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+			conversation = append(conversation, userMessage)
+		}
 
 		message, err := a.runInterface(ctx, conversation)
 		if err != nil {
@@ -46,14 +51,49 @@ func (a *Agent) Run(ctx context.Context) error {
 		}
 
 		conversation = append(conversation, message.ToParam())
+		toolResults := []anthropic.ContentBlockParamUnion{}
 
 		for _, content := range message.Content {
 			switch content.Type {
 			case "text":
 				fmt.Printf("\u001b[93mClaude\u001b[0m: %s\n", content.Text)
+			case "tool_use":
+				results := a.executeTool(content.ID, content.Name, content.Input)
+				toolResults = append(toolResults, results)
 			}
+
+			if len(toolResults) == 0 {
+				readUserInput = true
+				continue
+			}
+			readUserInput = false
+			conversation = append(conversation, anthropic.NewUserMessage(toolResults...))
 		}
 	}
 
 	return nil
+}
+
+func (a *Agent) executeTool(id, name string, input json.RawMessage) anthropic.ContentBlockParamUnion {
+	var toolDef ToolDefinition
+	var found bool
+
+	for _, tool := range a.tools {
+		if tool.Name == name {
+			toolDef = tool
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return anthropic.NewToolResultBlock(id, fmt.Sprintf("Tool %s not found", name), true)
+	}
+
+	fmt.Printf("\u001b[92mtool\u001b[0m: %s(%s)\n", name, input)
+	response, err := toolDef.Function(input)
+	if err != nil {
+		return anthropic.NewToolResultBlock(id, err.Error(), true)
+	}
+	return anthropic.NewToolResultBlock(id, response, false)
 }
